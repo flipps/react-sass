3fdd64df6fd0cf3e92557681a5dc38fb348d6a35
Filipe Morais <fi.morais.mail@gmail.com>
2018-07-24 18:26:45 -0300

    Testing scss files and flexbox-grid-mixins.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            <Button msg="Click Me"/>
+          </p>
+        </div>
+      </Fragment>
     );
   }
 }
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './button.scss';
+
+const Button = ({ msg }) =>
+    <button className="btn">{ msg }</button>
+
+export default Button;
