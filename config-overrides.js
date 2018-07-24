e20a26c4652277f3b2d08bee2a40246db7fdd77f
Filipe Morais <fi.morais.mail@gmail.com>
2018-07-24 16:00:30 -0300

    Testing scss files compilation.

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,6 @@
-const rewireSass = require('react-app-rewire-scss');
+const rewireSass = require("react-app-rewire-scss");
 
 module.exports = function override(config, env) {
-    config = rewireSass(config, env);
-    return config;
-}
+  config = rewireSass(config, env);
+  return config;
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './App.scss';
+import './styles/App.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
